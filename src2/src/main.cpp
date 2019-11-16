d6de95e315b112f14c4d71be6a0ef1e30eedfd40
Luigi Massacci <luigi.massacci.it@gmail.com>
2019-11-16 07:43:42 +0000

    resructured main

--- src2/src/main.cpp
+++ src2/src/main.cpp
@@ -14,13 +14,13 @@
 int main()
 {	
 	Image img(400, 400); //ARG: height width
-	img.load_data("/home/luigi/source/repos/rl_2019-20/src/Data/color_data.txt");
-
   	Line line;
-	Rig rig(10); //ARG: number of point for the skeleton
+	Rig rig(10); //ARG: number of points for the skeleton
+	Benchmark benchmark("run_data.txt"); //ARG: file to save run data
+	
 	Graphics graphics("feed"); //ARG: window name
 
-	Benchmark benchmark("")
+	img.load_data("/home/luigi/source/repos/rl_2019-20/src/Data/color_data.txt");
 	
 	bool camera_opened = true;
     Camera camera(camera_opened, 30, 400, 400);
@@ -28,68 +28,39 @@ int main()
 		std::cout << "Camera unavailable" << std::endl;
 	else
 	{
-		cv::Mat processed_frame;
-
 		bool silver_found = false;
 		
 		int img_number = 1;
-		int arr[2];
 		while(camera_opened && !silver_found)
 		{
-		//	cv::setMouseCallback("ciccio", CallBackFunc, arr);
-
-			std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 			
+			benchmark.start_clock();
+			//IMAGE PROCESSING
 			camera.fillFrame(img.getImage(), ++img_number);		
-
 			line.clear();  
 			analyse(img, line);
-			count_corners(line, img.visited, img.height(), img.width(), 20);
+			count_corners(line, img.visited, img.height(), img.width(), 20);		
+			greenRegionsPosition(img, line);
 			rig.make_rig(line.pixels_list, line.num_nodes);
-
-			// for (int i = 0; i < rig.num_points; ++i)
-			// 	std::cout << rig.right_points[i].first << " " << rig.right_points[i].second << "\n";
-
-			std::cout << line.num_nodes/(2*rig.num_points) << std::endl;
-
-			std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
-			std::cout << "It took me " << time_span.count() << " seconds."  << std::endl;
-
-			processed_frame = img.copy();
+			
+			//CALCULATING ERROR
+			//...
+			
+			benchmark.stop_clock();
+			
+			
+			//DEMO-DBG
+			line.show_data();
 			graphics.draw(img.getImage());
 			cv::waitKey(0);
-
-			
-			std::cout << "Numero di nodi neri:  " << line.num_nodes << std::endl;
-			std::cout << "Numero di vertici:    " << line.num_corners << std::endl;
-			std::cout << "Numero regioni verdi: " << img.green_regions.size() << std::endl;
-			for (auto i : img.green_regions)
-				std::cout << "barycentre " << i.first << i.second << std::endl;
-
-			greenRegionsPosition(img, line);
-
-			for(int i = 0; i < 2; ++i)
-			{
-				for(int j = 0; j < 2; ++j)
-					std::cout << line.greenPos[i][j] << " ";
-
-				std::cout << std::endl;
-			}
-
-			
-
-
-			
+			cv::Mat processed_frame = img.copy();
 			graphics.outline(processed_frame, img.visited, img.green_regions);
 		//	graphics.surface(processed_frame, img.visited, img);
 			graphics.apply_rig(processed_frame, rig);
 			graphics.draw(processed_frame);
-
 			cv::waitKey(0);
-			
-			
 		}
+		benchmark.save();
 	}
     
 
